3c82275b18495ded09d7589ae0472ad13a5d5780
Hisamat7 <muhammedhisam10@gmail.com>
2025-02-03 20:22:50 +0530

    passed review to vendor

--- lib/venders/VendorScreen/ReviewScreen/VendorReviewScreen.dart
+++ lib/venders/VendorScreen/ReviewScreen/VendorReviewScreen.dart
@@ -16,9 +16,11 @@ class _VendorReviewsPageState extends State<VendorReviewsPage> {
     print(vendorId);
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        surfaceTintColor: Colors.transparent,
         title: Text(
-          "Vendor Reviews",
-          style: TextStyle(fontSize: 20.sp, fontWeight: FontWeight.bold),
+          "User Reviews",
+          style: TextStyle(fontSize: 20.sp, color: Colors.teal[900]),
         ),
       ),
       body: Padding(
@@ -38,36 +40,60 @@ class _VendorReviewsPageState extends State<VendorReviewsPage> {
                   if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
                     return Center(child: Text("No reviews yet."));
                   }
-
-                  print("Total reviews: ${snapshot.data!.docs.length}");
-
                   var reviews = snapshot.data!.docs;
 
                   return Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      Text(
-                        "Total Reviews: ${reviews.length}",
-                        style: TextStyle(fontSize: 18.sp, fontWeight: FontWeight.w600),
+                      SizedBox(height: 20.h),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 10.0),
+                        child: Text(
+                          "Total Reviews  ${reviews.length}",
+                          style: TextStyle(
+                              fontSize: 20.sp, color: Colors.teal[700]),
+                        ),
                       ),
-                      SizedBox(height: 10.h),
+                      SizedBox(height: 30.h),
                       Expanded(
                         child: ListView.builder(
                           itemCount: reviews.length,
                           itemBuilder: (context, index) {
                             var review = reviews[index];
-                            return Card(
-                              margin: EdgeInsets.symmetric(vertical: 8.h),
-                              child: ListTile(
-                                leading: Icon(Icons.reviews, color: Colors.teal),
-                                title: Text(review["username"] ?? "Anonymous"),
-                                subtitle: Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    Text("Rating: ${review["rating"]}/5"),
-                                    Text("Comment: ${review["review"]}")
-                                  ],
+                            return Column(
+                              children: [
+                                ListTile(
+                                  leading: CircleAvatar(
+                                    backgroundColor:
+                                        Color.fromARGB(255, 54, 219, 142),
+                                    radius: 20,
+                                    child: Text(
+                                      review['username'][0],
+                                      style: TextStyle(color: Colors.teal[700]),
+                                    ), // First letter of username
+                                  ),
+                                  title: Text(
+                                    review['username'],
+                                    style: TextStyle(
+                                        color: Colors.teal[900], fontSize: 16),
+                                  ), // Display username
+                                  subtitle: Text(
+                                    "Rating : ${review['rating']}",
+                                    style: TextStyle(color: Colors.teal[900]),
+                                  ),
                                 ),
-                              ),
+                                Padding(
+                                  padding: const EdgeInsets.only(
+                                      left: 16, right: 16),
+                                  child: Text(
+                                    review['review'],
+                                    style: TextStyle(color: Colors.teal[800]),
+                                  ),
+                                ),
+                                Divider(
+                                  height: 20.h,
+                                )
+                              ],
                             );
                           },
                         ),
